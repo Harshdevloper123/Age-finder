630a8418c54cd6bf89181cd9bdb9746137dc7d2c
Harshdevloper123 <159701917+Harshdevloper123@users.noreply.github.com>
2024-02-24 11:14:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("Age Detecter , click ok to start")
+console.log("Age Decter welcome you")
+var a = prompt("Enter your birth year");
+console.log("Given year is") 
+console.log(a)
+confirm(a)
+var b = (2024);
+var c = (b - a)
+
+if(a==''){
+    alert("are you kidding pls enter birth year")
+}
+else {
+    confirm(
+        "click on ok to see your age"
+    )
+
+    console.log("your age is currently about about ")
+    console.log(c)
+    alert(c)  
+}
+alert("age decter is op")
+console.log("Age decter is op")
